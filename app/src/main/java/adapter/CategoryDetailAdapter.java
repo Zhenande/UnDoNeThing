42b3c9d732834a66e575633c9c13ec97f29165d5
Zhenande <lieumanh96@gmail.com>
2018-04-25 22:04:30 +0700

    New Update

---
+++ app/src/main/java/adapter/CategoryDetailAdapter.java
@@ -0,0 +1,62 @@
+package adapter;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.abc.khoaluan.R;
+
+import java.util.ArrayList;
+
+import holder.CategoryDetailViewHolder;
+import model.CategoryDetail;
+
+
+
+public class CategoryDetailAdapter extends RecyclerView.Adapter<CategoryDetailViewHolder> {
+
+    private ArrayList<CategoryDetail> list;
+
+    public CategoryDetailAdapter(ArrayList<CategoryDetail> data) {
+        this.list = data;
+    }
+
+    @NonNull
+    @Override
+    public CategoryDetailViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.listview_category_detail_row,parent,false);
+        LinearLayout ll = view.findViewById(R.id.category_detail_row_linear);
+        ImageView iw = view.findViewById(R.id.listview_category_detail_row_image);
+        WindowManager wm = (WindowManager)parent.getContext().getSystemService(Context.WINDOW_SERVICE);
+        Point size = new Point();
+        wm.getDefaultDisplay().getRealSize(size);
+        ll.getLayoutParams().width = 3*size.x/4;
+        ll.getLayoutParams().height = size.y/6;
+        iw.getLayoutParams().width = 3*size.x/4-(size.x/2);
+        iw.getLayoutParams().height = size.y/6;
+        CategoryDetailViewHolder holder = new CategoryDetailViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryDetailViewHolder holder, int position) {
+        holder.CategoryDetailName.setText(list.get(position).getCategoryDetailName());
+        holder.CategoryDetailAddress.setText(list.get(position).getCategoryDetailAddress());
+        holder.CategoryDetailImage.setImageResource(list.get(position).getCategoryDetailImageResourceId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
---
+++ app/src/main/java/adapter/MainHorizontalAdapter.java
@@ -0,0 +1,73 @@
+package adapter;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.abc.khoaluan.R;
+
+import java.util.ArrayList;
+
+import holder.MainHorizontalViewHolder;
+import model.CardModel;
+
+/**
+ * Created by ABC on 3/6/2018.
+ */
+
+public class MainHorizontalAdapter extends RecyclerView.Adapter<MainHorizontalViewHolder> {
+
+    private ArrayList<CardModel> list;
+
+
+    public MainHorizontalAdapter(ArrayList<CardModel> Data){
+        list = Data;
+    }
+
+    @NonNull
+    @Override
+    public MainHorizontalViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycle_card_item,parent,false);
+
+        // Set the width of the Card View = Width(The Devices) / 4
+        // Set the height of the Card View = Height(The Devices) / 6
+        // Start
+        LinearLayout ll = view.findViewById(R.id.recycle_card_linearLayout);
+        WindowManager wm = (WindowManager) parent.getContext().getSystemService(Context.WINDOW_SERVICE);
+        Point size = new Point();
+        int width = ll.getLayoutParams().width;
+        if(width == 160) {
+            wm.getDefaultDisplay().getRealSize(size);
+            ll.getLayoutParams().width = size.x / 4;
+            ll.getLayoutParams().height = size.y / 6;
+            ll.requestLayout(); // Important !!! Need this when change the Layout of item dynamically
+        }
+        // End
+        MainHorizontalViewHolder holder = new MainHorizontalViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MainHorizontalViewHolder holder, int position) {
+
+        holder.RestaurentName.setText(list.get(position).getResName());
+        holder.RestaurentAddress.setText(list.get(position).getAddress());
+        holder.RestaurentImage.setImageResource(list.get(position).getResImageResourceId());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
